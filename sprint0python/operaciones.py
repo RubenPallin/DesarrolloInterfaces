b793855705d108c3066a05dbfb73cf6b82d55cb1
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2023-09-26 10:07:11 +0200

    Creadas las funciones de operaciones.py

--- sprint0python/operaciones.py
+++ sprint0python/operaciones.py
@@ -1,3 +1,20 @@
+num1 = 0;
+num2 = 0;
 
-def suma_numeros(a, b):
-	suma
+def suma_numeros(num1, num2):
+	suma = num1 + num2;
+	return suma;
+
+def resta_numeros(num1, num2):
+	resta = num1-num2;
+	return resta;
+
+def multiplicacion_numeros(num1, num2):
+	multiplicacion = num1*num2;
+	return multiplicacion;
+
+def division_numeros(num1, num2):
+    if num2 == 0:
+        return "No se puede dividir por cero.";
+    else:
+        return num1 / num2;
