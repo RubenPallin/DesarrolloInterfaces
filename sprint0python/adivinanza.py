332a26ac653ecf3e5150becaa8204f8b7d67c765
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2023-09-26 09:05:14 +0200

    El adivinanza.py funciona correctamente

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,1 +1,11 @@
-a
+print ("Soy un mes de vacaciones con nombre de emperador. A veces refresco el rostro, otras doy mucho calor.¿Quién soy?")
+print ("a) Junio");
+print ("b) Agosto");
+print ("c) Octubre");
+
+respuesta = input("Tu respuesta es...(a/b/c) ");
+
+if (respuesta == 'b'):
+    print("Respuesta correcta");
+else:
+    print("Respuesta incorrecta");
