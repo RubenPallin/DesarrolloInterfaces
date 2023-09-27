414666ab590f2a007a31fae7bcb300105fda8f2d
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2023-09-27 09:10:02 +0200

    Update operaciones.py

--- sprint0python/operaciones.py
+++ sprint0python/operaciones.py
@@ -1,20 +1,20 @@
-num1 = 0;
-num2 = 0;
+num1 = 0
+num2 = 0
 
 def suma_numeros(num1, num2):
-	suma = num1 + num2;
-	return suma;
+	suma = num1 + num2
+	return suma
 
 def resta_numeros(num1, num2):
-	resta = num1-num2;
-	return resta;
+	resta = num1 - num2
+	return resta
 
 def multiplicacion_numeros(num1, num2):
-	multiplicacion = num1*num2;
-	return multiplicacion;
+	multiplicacion = num1 * num2
+	return multiplicacion
 
 def division_numeros(num1, num2):
     if num2 == 0:
-        return "No se puede dividir por cero.";
+        return "No se puede dividir por cero."
     else:
-        return num1 / num2;
+        return num1 / num2
