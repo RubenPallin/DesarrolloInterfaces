b337fd12e3dbf89075bbe09631054861b43e6c47
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2023-09-27 10:09:59 +0200

    Modificado el adivinanza.py con la función random.sample

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,51 +1,59 @@
-puntuacion = 0;
+import random
 
-print ("Soy un mes de vacaciones con nombre de emperador. A veces refresco el rostro, otras doy mucho calor.¿Quién soy?")
-print ("a) Junio");
-print ("b) Agosto");
-print ("c) Octubre");
+numeros = list(range(1,4))
+puntuacion = 0
 
-respuesta = input("Tu respuesta es...(a/b/c) ");
+mat_adivinanza = random.sample(numeros, 2)
 
-if (respuesta.upper() == 'B'):
-    print("Respuesta correcta");
-    print("Has conseguido 10 puntos");
-    puntuacion += 10;
-else:
-    print("Respuesta incorrecta");
-    print("Has perdido 5 puntos");
-    puntuacion -= 5;
+if mat_adivinanza[0] == 1 or mat_adivinanza[1] == 1:
+    print ("Soy un mes de vacaciones con nombre de emperador. A veces refresco el rostro, otras doy mucho calor.¿Quién soy?")
+    print ("a) Junio")
+    print ("b) Agosto")
+    print ("c) Octubre")
 
-print ("Sal al campo por las noches si me quieres conocer, soy señor de grandes ojos, cara seria y gran saber. ¿Quién soy?")
-print ("a) Halcón");
-print ("b) Murciélago");
-print ("c) Búho");
+    respuesta = input("Tu respuesta es...(a/b/c) ")
 
-respuesta = input("Tu respuesta es...(a/b/c) ");
+    if (respuesta.upper() == 'B'):
+        print("Respuesta correcta")
+        print("Has conseguido 10 puntos")
+        puntuacion += 10
+    else:
+        print("Respuesta incorrecta")
+        print("Has perdido 5 puntos")
+        puntuacion -= 5
 
-if (respuesta.upper() == 'C'):
-    print("Respuesta correcta");
-    print("Has conseguido 10 puntos");
-    puntuacion += 10;
-else:
-    print("Respuesta incorrecta");
-    print("Has perdido 5 puntos");
-    puntuacion -= 5;
+if mat_adivinanza[0] == 2 or mat_adivinanza[1] == 2:
+    print ("Sal al campo por las noches si me quieres conocer, soy señor de grandes ojos, cara seria y gran saber. ¿Quién soy?")
+    print ("a) Halcón")
+    print ("b) Murciélago")
+    print ("c) Búho")
 
-print ("¿Qué cosa es? ¿Qué cosa es? Que corre mucho y no tiene pies.")
-print ("a) El viento");
-print ("b) El guepardo");
-print ("c) La ballena");
+    respuesta = input("Tu respuesta es...(a/b/c) ")
 
-respuesta = input("Tu respuesta es...(a/b/c) ");
+    if (respuesta.upper() == 'C'):
+        print("Respuesta correcta")
+        print("Has conseguido 10 puntos")
+        puntuacion += 10
+    else:
+        print("Respuesta incorrecta")
+        print("Has perdido 5 puntos")
+        puntuacion -= 5
 
-if (respuesta.upper() == 'A'):
-    print("Respuesta correcta");
-    print("Has conseguido 10 puntos");
-    puntuacion += 10;
-else:
-    print("Respuesta incorrecta");
-    print("Has perdido 5 puntos");
-    puntuacion -= 5;
+if mat_adivinanza[0] == 3 or mat_adivinanza[1] == 3: 
+    print ("¿Qué cosa es? ¿Qué cosa es? Que corre mucho y no tiene pies.")
+    print ("a) El viento")
+    print ("b) El guepardo")
+    print ("c) La ballena")
 
-print("Tu puntuación final ha sido de: "+str(puntuacion));
+    respuesta = input("Tu respuesta es...(a/b/c) ")
+
+    if (respuesta.upper() == 'A'):
+        print("Respuesta correcta")
+        print("Has conseguido 10 puntos")
+        puntuacion += 10
+    else:
+        print("Respuesta incorrecta")
+        print("Has perdido 5 puntos")
+        puntuacion -= 5
+
+print("Tu puntuación final ha sido de: "+str(puntuacion))
