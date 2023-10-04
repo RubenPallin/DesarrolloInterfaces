36ffeeed4f7ccdd86ad278d383207fb1aa92c59e
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2023-10-04 09:55:10 +0200

    Update window.py

--- sprint1gtk/dialog/window.py
+++ sprint1gtk/dialog/window.py
@@ -1,1 +1,9 @@
+from tkinter import tkk, Button
 
+class MainWindow:
+    def on_button_click(sef):
+        pass
+    def __init__(self, root):
+        self.root = root
+        self.button = tkk.Button(self.root, text="Realizar acción", command= self.on_button_click)
+        self.button.pack()
