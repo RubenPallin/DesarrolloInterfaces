977296f1bc3eb8117b6818496117132bb41888db
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2023-10-11 13:15:28 +0200

    Comentado main.py

--- sprint1gtk/catalog/main.py
+++ sprint1gtk/catalog/main.py
@@ -2,6 +2,6 @@ from tkinter import Tk
 from window import MainWindow
 
 if __name__== "__main__":
-    root = Tk()
-    app = MainWindow(root)
-    root.mainloop()
+    root = Tk()  # Crea una instancia de la clase "Tk" que representa la ventana principal de la aplicación
+    app = MainWindow(root)  # Crea una instancia de la clase "MainWindow" y pasa la ventana raíz "root" como argumento
+    root.mainloop()  # Inicia el bucle principal de la aplicación, que mantiene la ventana abierta y responde a eventos
