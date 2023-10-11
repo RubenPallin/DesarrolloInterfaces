d1d1cef876dc3188af172d8fa57a2d389023e97b
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2023-10-11 13:13:03 +0200

    Comentado detail_window.py

--- sprint1gtk/catalog/detail_window.py
+++ sprint1gtk/catalog/detail_window.py
@@ -3,18 +3,24 @@ from tkinter import ttk
 
 class DetailWindow:
 
+    # Constructor de la clase
     def __init__(self, root, title, image, description):
+        # Inicializa la ventana de detalles con la información proporcionada
+        self.root = root  
+        self.title = title  
+        self.image = image  
+        self.description = description  
+        
+        # Crea una nueva ventana secundaria (ventana de detalles)
+        self.window = tk.Toplevel(root)  # Crea una ventana secundaria que se abre en la ventana raíz
+        self.window.title(self.title)  # Establece el título de la ventana de detalles con el título del personaje
 
-        self.root = root
-        self.title = title
-        self.image = image
-        self.description = description
-        self.window = tk.Toplevel(root)
-        self.window.title(self.title)
+        # Crea etiquetas para mostrar la imagen, el título y la descripción del personaje
+        image_label = ttk.Label(self.window, image=self.image)
+        image_label.pack()  
 
-        image_label = ttk.Label(self.window, image = self.image)
-        image_label.pack()
-        title_label = ttk.Label(self.window, text = self.title, font=("JetBrains mono", 16))
-        title_label.pack()
-        description_label = ttk.Label(self.window, text = self.description, wraplength = 300)
-        description_label.pack()
+        title_label = ttk.Label(self.window, text=self.title, font=("JetBrains mono", 16))  
+        title_label.pack()  
+
+        description_label = ttk.Label(self.window, text=self.description, wraplength=300) 
+        description_label.pack() 
