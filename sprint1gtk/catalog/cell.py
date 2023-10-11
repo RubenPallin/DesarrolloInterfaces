a0383c1fd4862bce2bd121e0451fd39c2ff48f19
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2023-10-11 11:45:08 +0200

    Editado cell.py para el ej 6

--- sprint1gtk/catalog/cell.py
+++ sprint1gtk/catalog/cell.py
@@ -2,7 +2,14 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 class Cell:
-    def __init__(self, title, path):
+    def __init__(self, title, image_path, description):
         self.title = title
-        self.path = path
-        self.image_tk = ImageTk.PhotoImage(file = self.path)
+        self.path = image_path
+        self.description = description
+
+    
+        resizedImage = (Image.open(self.path)).resize((100, 100), Image.Resampling.LANCZOS)
+        self.image_tk = ImageTk.PhotoImage(resizedImage)
+
+        # Convierte la imagen redimensionada en un objeto ImageTk para usar en Tkinter
+        self.imageTk = ImageTk.PhotoImage(resizedImage)
