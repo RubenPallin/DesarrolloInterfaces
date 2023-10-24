834b75df4b0f6f21ec6c8f5c18232750d2853633
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2023-10-24 09:10:10 +0200

    Modificado Cell.py para el EJERCICIO 3

--- sprint2catalog/catalog/cell.py
+++ sprint2catalog/catalog/cell.py
@@ -1,13 +1,41 @@
 import tkinter as tk
+import requests
 from PIL import Image, ImageTk
 
-class Cell:
-    # Constructor de la clase
-    def __init__(self, title, image_path, description):
-        self.title = title  
-        self.image_path = image_path  
-        self.description = description  
 
-        # Abre la imagen, la redimensiona a 100x100 píxeles y la almacena en "self.image_tk"
-        resized_image = Image.open(self.image_path).resize((100, 100), Image.LANCZOS)
-        self.image_tk = ImageTk.PhotoImage(resized_image)
+class Cell:
+    def __init__(self, title, image_url, description):
+        self.title = title
+        self.image_path = self.download_image(image_url)  # Descarga la imagen y obtiene la ruta local
+        self.description = description
+        self.load_image()
+
+    def download_image(self, image_url):
+        try:
+            response = requests.get(image_url)
+            response.raise_for_status()
+
+            if response.status_code == 200:
+                image_data = response.content
+                image_filename = "local_image.jpg"  # Puedes definir un nombre de archivo local
+                with open(image_filename, "wb") as image_file:
+                    image_file.write(image_data)
+                return image_filename
+            else:
+                print(f"Error al descargar la imagen: {response.status_code}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error al cargar la imagen: {e}")
+        return None
+
+    def load_image(self):
+        if self.image_path:
+            try:
+                image = Image.open(self.image_path)
+                # Redimensiona la imagen a 100x100 píxeles
+                image = image.resize((100, 100), Image.LANCZOS)
+                self.image_tk = ImageTk.PhotoImage(image)
+            except Exception as e:
+                print(f"Error al cargar la imagen: {e}")
+                self.image_tk = None
+        else:
+            self.image_tk = None
