f84b679bc69904940a517b23774005d8c4480844
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2023-10-25 09:20:42 +0200

    Modificado main.py para que muestre el loading y ejercicios en adelante.

--- sprint2catalog/catalog/main.py
+++ sprint2catalog/catalog/main.py
@@ -1,7 +1,9 @@
+# Importamos las bibliotecas necesarias
 from tkinter import Tk
 from window import MainWindow
+from load_window import LoadingWindow
 
 if __name__== "__main__":
     root = Tk()  # Crea una instancia de la clase "Tk" que representa la ventana principal de la aplicación
-    app = MainWindow(root)  # Crea una instancia de la clase "MainWindow" y pasa la ventana raíz "root" como argumento
+    app = LoadingWindow(root)  # Crea una instancia de la clase "LoadingWindow" y pasa la ventana raíz "root" como argumento
     root.mainloop()  # Inicia el bucle principal de la aplicación, que mantiene la ventana abierta y responde a eventos
